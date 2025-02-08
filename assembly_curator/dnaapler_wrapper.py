6d60b8d72cc5ce43df44526f07aa789591ec6251
MrTomRod <roder.thomas@gmail.com>
2025-02-08 01:39:14 +0100

    Dnaapler (not implemented yet into workflow)

--- assembly_curator/dnaapler_wrapper.py
+++ assembly_curator/dnaapler_wrapper.py
@@ -1,206 +1,185 @@
 import os
-import shutil
-from pathlib import Path
-
 import click
-from click.testing import CliRunner
+import logging
+import subprocess
+from tempfile import TemporaryDirectory
+import pandas as pd
+from Bio.Seq import reverse_complement
 
-# Create a dummy click context
-def create_dummy_context():
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        ctx = click.Context(click.Command('dummy'))
-        ctx.params = {
-            'input': 'dummy_input',
-            'output': 'dummy_output',
-            'threads': 4,
-            'prefix': 'dummy_prefix',
-            'evalue': 1e-10,
-            'force': True,
-            'autocomplete': 'none',
-            'seed_value': 13,
-            'ignore': None,
-            'db': 'all',
-            'custom_db': '',
-        }
-        return ctx
+CPU_COUNT = os.cpu_count()
 
-# Example usage
-ctx = create_dummy_context()
-
-from loguru import logger
-
-from dnaapler import all
-from dnaapler.utils.all import all_process_blast_output_and_reorient
-from dnaapler.utils.bulk import bulk_process_blast_output_and_reorient, run_bulk_blast
-from dnaapler.utils.cds_methods import (
-    run_blast_based_method,
-    run_largest,
-    run_mystery,
-    run_nearest,
-)
-from dnaapler.utils.constants import DNAAPLER_DB
-from dnaapler.utils.external_tools import ExternalTool
-from dnaapler.utils.util import (
-    begin_dnaapler,
-    check_duplicate_headers,
-    end_dnaapler,
-    get_version,
-    print_citation,
-    run_autocomplete,
-)
-from dnaapler.utils.validation import (
-    check_evalue,
-    instantiate_dirs,
-    validate_choice_autocomplete,
-    validate_choice_db,
-    validate_choice_mode,
-    validate_custom_db_fasta,
-    validate_fasta,
-    validate_fasta_all,
-    validate_fasta_bulk,
-    validate_ignore_file,
-)
+MESSAGE_DICT = {
+    'Contig_already_reoriented': 'already-reoriented',
+    'No_MMseqs2_hits': 'failed-no-hits',
+}
 
 
-def all(
-        input='data/15_N/lja/assembly.fasta',
-        output='data/15_N/lja/dnaapler',
-        threads=8,
-        prefix='prefix',
-        evalue=1e-10,
-        force=True,
-        autocomplete='none',
-        seed_value=13,
-        ignore='',  # Text file listing contigs (one per row) that are to be ignored
-        db='all',
-        custom_db='',
-):
-    """Reorients contigs to begin with any of dnaA, repA, terL or archaeal COG1474 Orc1/cdc6"""
+def read_fasta(fasta_file) -> dict:
+    # returns {header: {header: header, seq: sequence}}
+    with open(fasta_file) as f:
+        sequences = [s for s in f.read().lstrip().split('>') if s]
+    sequences = [s.split('\n', 1) for s in sequences]
+    sequences = [(h.split(' ', 1)[0], dict(header=h, seq=s)) for h, s in sequences]
+    contig_ids = set([s[0] for s in sequences])
+    assert len(contig_ids) == len(sequences), f"Non-unique contig_ids in {fasta_file}: {contig_ids=}"
+    return dict(sequences)
 
-    # validates the directory  (need to before I start dnaapler or else no log file is written)
-    instantiate_dirs(output, force)
 
-    params = {
-        "--input": input,
-        "--output": output,
-        "--threads": threads,
-        "--force": force,
-        "--prefix": prefix,
-        "--evalue": evalue,
-        "--autocomplete": autocomplete,
-        "--seed_value": seed_value,
-        "--ignore": ignore,
-        "--custom_db": custom_db,
-        "--force": force,
-        "--db": db,
-    }
+def write_fasta(fasta: dict, output_file: str, replace_newlines: bool = True):
+    with open(output_file, 'w') as f:
+        for scf_id in fasta:
+            header = fasta[scf_id]['header'].strip()
+            sequence = fasta[scf_id]['seq'].strip()
+            if replace_newlines:
+                sequence = sequence.replace('\n', '')
+            f.write(f">{header}\n{sequence}\n")
 
-    # defines gene
-    gene = "all"
 
-    # other options
+def read_output_table(output_table: str) -> pd.DataFrame:
+    return pd.read_csv(output_table, sep='\t', index_col=0, dtype=str)
 
-    if db == "dnaa":
-        gene = "dnaA"
-    elif db == "repa":
-        gene = "repA"
-    elif db == "terl":
-        gene = "terL"
-    elif db == "dnaa,repa":
-        gene = "dnaA,repA"
-    elif db == "dnaa,terl":
-        gene = "dnaA,terL"
-    elif db == "repa,terl":
-        gene = "repA,terL"
-    elif db == "cog1474":
-        gene = "cog1474"
 
-    # custom
-    if custom_db != "":
-        gene = "custom"
+def run_dnaapler(
+        input_fasta,
+        workdir: str = None,
+        n_cpu: int = None,
+        arguments: [str] = ['-t', str(CPU_COUNT), '-f']
+) -> (dict, pd.DataFrame):
+    """
+    Run dnaapler on the input fasta file and return the reoriented fasta and the reorientation summary table.§
+    :param input_fasta: Path to the input fasta file
+    :param workdir: Path to the output directory
+    :param n_cpu: Number of CPUs to use
+    :param arguments: Additional arguments to pass to dnaapler
+    :return: (fasta as dict, output_table as pd.DataFrame)
+    """
+    if n_cpu is None:
+        n_cpu = os.cpu_count()
+    assert type(n_cpu) is int, f"n_cpu must be an integer, not {type(n_cpu)}"
 
-    # initial logging etc
-    # start_time = begin_dnaapler(input, output, threads, gene, params)
-
-    # validates fasta
-    validate_fasta_all(input)
-    check_duplicate_headers(input)
-
-    # validate e value
-    check_evalue(evalue)
-
-    # create flag for ignore
-    if ignore == "":
-        ignore_flag = False
+    if workdir:
+        os.makedirs(workdir, exist_ok=True)
     else:
-        ignore_flag = True
-    # checks if the ignore file exists and contains text
-    if ignore_flag == True:
-        logger.info(f"You have specified contigs to ignore in {ignore}.")
-        exists_contains_txt = validate_ignore_file(ignore)
+        tempdir = TemporaryDirectory()
+        workdir = tempdir.name
 
-    if gene == "custom":
-        # validates custom fasta input for database
-        validate_custom_db_fasta(Path(custom_db))
+    input_fasta = os.path.abspath(input_fasta)
+    input_fasta_temp = os.path.join(workdir, 'input.fasta')
+    os.symlink(input_fasta, input_fasta_temp)
 
-        # make db
-        db_dir = os.path.join(output, "custom_db")
-        Path(db_dir).mkdir(parents=True, exist_ok=True)
-        custom_db_fasta = os.path.join(db_dir, "custom_db.faa")
-        shutil.copy2(custom_db, custom_db_fasta)
+    arguments = [
+        'dnaapler', 'all',
+        '-i', 'input.fasta',
+        '-o', 'out',
+        *arguments
+    ]
 
-        logdir = Path(f"{output}/logs")
+    logging.info(f"Running dnaapler in {workdir=}")
+    logging.info(f"Running dnaapler with {arguments=}")
+    logging.info(f"Running dnaapler with arguments={' '.join(arguments)}")
 
-        # custom db
-        # make custom db
-        custom_database = os.path.join(db_dir, "custom_db")
-        makeblastdb = ExternalTool(
-            tool="makeblastdb",
-            input=f"-in {custom_db_fasta}",
-            output=f"-out {custom_database}",
-            params="-dbtype prot ",
-            logdir=logdir,
-        )
+    res = subprocess.run(arguments, cwd=workdir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    error_msg = f"Error running dnaapler: {res.returncode}\n{res.stdout.decode()}\n{res.stderr.decode()}"
+    assert res.returncode == 0, error_msg
+    output_fasta = os.path.join(workdir, 'out', 'dnaapler_reoriented.fasta')
+    assert os.path.isfile(output_fasta), error_msg
+    output_table = os.path.join(workdir, 'out', 'dnaapler_all_reorientation_summary.tsv')
+    assert os.path.isfile(output_table), error_msg
 
-        ExternalTool.run_tool(makeblastdb, ctx)
-    else:
-        custom_db = None
+    fasta = read_fasta(output_fasta)
+    output_table = read_output_table(output_table)
 
-    # runs bulk BLAST
-    run_bulk_blast(
-        ctx, input, output, prefix, gene, evalue, threads, custom_db=custom_db
-    )
+    logging.info(f"Finished dnaapler. Table:\n\n{output_table.to_string()}\n")
 
-    # rerorients blast
-    blast_file = os.path.join(output, f"{prefix}_blast_output.txt")
+    if workdir is None:
+        tempdir.cleanup()
 
-    ### ignore
-    # list is empty
-    ignore_list = []
-    if ignore_flag == True:
-        if exists_contains_txt is False:
-            logger.warning(f"{ignore} contains no text. No contigs will be ignored")
+    return fasta, output_table
+
+
+def merge_fastas(
+        fasta_orig: dict,
+        fasta_dnaapler: dict,
+        output_table: pd.DataFrame,
+        default_topology: str = 'raise'
+) -> dict:
+    # make sure both dictionaries have the same keys
+    assert list(fasta_orig.keys()) == list(fasta_dnaapler.keys()), (
+        f"Keys do not match!\n"
+        f"{list(fasta_orig.keys())} != {list(fasta_dnaapler.keys())}")
+
+    assert default_topology in ['raise', 'linear', 'circular'], f"Unknown default_topology: {default_topology}"
+
+    for scf_id in fasta_orig:
+        row = output_table.loc[fasta_orig[scf_id]['header']]
+        logging.info(f'{scf_id}: Dnaapler result: {row.Gene_Reoriented}')
+
+        if not row.Start.isdigit():
+            assert row.Gene_Reoriented in MESSAGE_DICT, f"Unknown dnaapler result: {row.Gene_Reoriented}\n{row=}"
+            msg = MESSAGE_DICT[row.Gene_Reoriented]
+            fasta_orig[scf_id]['header'] += f' [dnaapler={msg}]'
+            continue
+
+        compressed_header = fasta_orig[scf_id]['header'].replace(' ', '')
+
+        def process_linear():
+            fasta_orig[scf_id]['seq'] = reverse_complement(fasta_orig[scf_id]['seq'])
+            fasta_orig[scf_id]['header'] += f' [dnaapler=reverse-complement] [dnaapler-gene={row.Gene_Reoriented}]'
+
+        def process_circular():
+            fasta_orig[scf_id]['seq'] = fasta_dnaapler[scf_id]['seq']
+            fasta_orig[scf_id]['header'] += f' [dnaapler=rotated] [dnaapler-gene={row.Gene_Reoriented}]'
+
+        if '[topology=linear]' in compressed_header:
+            process_linear()
+        elif '[topology=circular]' in compressed_header:
+            process_circular()
+        elif default_topology == 'linear':
+            logging.warning(f'Topology not specified in header, falling back to default: {default_topology}')
+            process_linear()
+        elif default_topology == 'circular':
+            logging.warning(f'Topology not specified in header, falling back to default: {default_topology}')
+            process_circular()
         else:
-            # gets all contigs in the ignore
-            # will split by space so short_contig only (to match BLAST)
-            with open(ignore) as f:
-                ignore_dict = {x.rstrip().split()[0] for x in f}
-            ignore_list = list(ignore_dict)
-
-    all_process_blast_output_and_reorient(
-        input,
-        blast_file,
-        output,
-        prefix,
-        ignore_list,
-        autocomplete,
-        seed_value,
-        custom_db=custom_db,
-    )
-
-    # end dnaapler
-    # end_dnaapler(start_time)
+            logging.error(f"Unknown topology in >{fasta_orig[scf_id]['header']}")
+            raise ValueError(f"Unknown topology in {fasta_orig[scf_id]['header']}")
+    return fasta_orig
 
 
-all()
+def run_all(
+        input_fasta,
+        output_fasta,
+        replace_newlines: bool = True,
+        default_topology: str = 'raise',
+        dnaapler_arguments: [str] = ['-t', str(CPU_COUNT), '-f'],
+        n_cpu=None,
+        workdir=None
+):
+    fasta_orig = read_fasta(input_fasta)
+    fasta_dnaapler, output_table = run_dnaapler(input_fasta, workdir, n_cpu, dnaapler_arguments)
+    fasta_result = merge_fastas(fasta_orig, fasta_dnaapler, output_table, default_topology)
+    write_fasta(fasta_result, output_fasta, replace_newlines)   
+
+
+@click.command()
+@click.argument('input_fasta', type=click.Path(exists=True))
+@click.argument('output_fasta', type=click.Path())
+@click.option('--replace-newlines/--no-replace-newlines', default=True)
+@click.option('--default-topology', type=click.Choice(['raise', 'linear', 'circular']),
+              default='raise', help='Default topology')
+@click.option('--dnaapler-arguments', type=str, default=f'-t {CPU_COUNT} -f',
+              help='Additional arguments to pass to dnaapler')
+@click.option('--n-cpu', type=int, default=None, help='Number of CPUs to use')
+@click.option('--workdir', type=click.Path(), default=None, help='Path to the output directory')
+def cli(input_fasta, output_fasta, replace_newlines, default_topology, workdir, dnaapler_arguments, n_cpu):
+    """
+    Run dnaapler on the input FASTA file and save the reoriented FASTA to the output file.
+    """
+    dnaapler_arguments = dnaapler_arguments.split()
+
+    logging.basicConfig(level=logging.INFO)
+    run_all(input_fasta, output_fasta, replace_newlines, default_topology, dnaapler_arguments, n_cpu, workdir)
+
+
+if __name__ == '__main__':
+    cli()
